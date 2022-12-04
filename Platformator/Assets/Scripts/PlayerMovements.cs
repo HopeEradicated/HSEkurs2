e01d2c4f2615a37b3d42bf6d1652689a93d7ca51
Lul1ch <84387626+Lul1ch@users.noreply.github.com>
2022-12-04 20:47:38 +0300

    Changed PlayerMovement physic and added new features

--- Platformator/Assets/Scripts/PlayerMovements.cs
+++ Platformator/Assets/Scripts/PlayerMovements.cs
@@ -6,8 +6,10 @@ public class PlayerMovements : MonoBehaviour
     Transform playerCoords;
     Rigidbody2D playerRb;
 
-    private float vForce = 500f/*Мб поменять название*/, hForce = 2f;
-    private bool canMove;
+    private float vForce = 500f/*Мб поменять название*/, hForce = 2f, plSpeed = 6f;
+    private float hDirection = 0f, wallDirIndex = -0.1f;
+    private float wallCheckDist = 2f;
+    private bool canMove, canJump, isOnWall;
 
     private void Start() {
         playerCoords = GetComponent<Transform>();
@@ -15,34 +17,54 @@ public class PlayerMovements : MonoBehaviour
     }
 
     private void Update() {
-        if (canMove){
+        if (canJump){
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)){
                 Jump();
-                canMove = false;
-            }
-
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
-                StrafeToTheRight();
-            }
-
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-                StrafeToTheLeft();
+                canJump = false;
+            } else if (isOnWall){
+                //Могут возникать проблемы, если на карте будут соприкасаться платформы и стены. Потенциально - поменять
+                if (playerRb.velocity.y < plSpeed){
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, wallDirIndex*plSpeed);
+                }
             }
         }
+        hDirection = Input.GetAxisRaw("Horizontal") * plSpeed;
+        
+       
+        /*Оставить, как альтернативную физику движения, для другого персонажа или класса, например
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            StrafeToTheRight();
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            StrafeToTheLeft();
+        }
+        */
+
         if (Input.GetKey(KeyCode.F)){
             playerCoords.localPosition = Vector3.Lerp(playerCoords.localPosition, 
             new Vector3(0, 1f,0), 5f);
         }
     }
 
+    private void FixedUpdate() {
+        Physics2D.queriesStartInColliders = false;
+        if (Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, wallCheckDist).collider != null && hDirection == -1) {
+            playerRb.velocity = new Vector2(hDirection, playerRb.velocity.y);
+        } else if (Physics2D.Raycast(transform.position, Vector2.left * transform.localScale.x, wallCheckDist).collider != null && hDirection == 1) {
+            playerRb.velocity = new Vector2(hDirection, playerRb.velocity.y);
+        } else if (Physics2D.Raycast(transform.position, Vector2.left * transform.localScale.x, wallCheckDist).collider == null && Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, wallCheckDist).collider == null) {
+            playerRb.velocity = new Vector2(hDirection, playerRb.velocity.y);
+        }
+
+    }
+
     private void Jump(){
         playerRb.AddForce(playerCoords.up * vForce);
-        //playerCoords.localPosition = Vector2.MoveTowards( playerCoords.localPosition, new Vector2(playerCoords.localPosition.x, playerCoords.localPosition.y + 3), vSpeed * Time.deltaTime);  
     }
 
     private void StrafeToTheRight(){
         playerRb.AddForce(playerCoords.right * hForce);
-        //playerCoords.localPosition = Vector2.MoveTowards( playerCoords.localPosition, new Vector2(newHorCoords, playerCoords.localPosition.y), hSpeed * Time.deltaTime);
     }
 
     private void StrafeToTheLeft(){
@@ -50,11 +72,23 @@ public class PlayerMovements : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.tag == "Ground" && !canMove){
-            canMove = true;
+        if (other.gameObject.tag == "Ground" && (!canMove || !canJump)){
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Wall"){
+            canJump = true;
+            isOnWall = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag == "Wall"){
+            isOnWall = false;
         }
     }
 }
 
-//Почему игрок уезжает вправо? Потому что в определённый момент сила даётся куда-то в пизду и он не понимает, что нужно прыгать. UPD: он просто крутиться, пофиксить джамп
 //Идея: сделать зацеп, когда игрок касается вертикальной части платформы
